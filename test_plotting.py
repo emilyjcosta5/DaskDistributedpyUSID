0436e3bfa95a545942a3762afca5ccbe58bbaf42
emilyjcosta5 <Pa$$word5>
2019-07-29 11:01:40 -0400

    Added test script for plotting benchmarks

---
+++ test_plotting.py
@@ -0,0 +1,7 @@
+from functions import plot_compute_times
+times_vec = [5,8,10]
+cpu_vec = [1,3,5]
+com_vec = ['Serial','Parallel','Dask']
+
+plot_compute_times(cpu_vec,times_vec,com_vec,png_name='test')
+
